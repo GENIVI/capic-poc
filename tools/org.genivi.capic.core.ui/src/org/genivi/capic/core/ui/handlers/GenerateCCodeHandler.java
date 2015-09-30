80a9c892798183882634dcd72b138ffdda4b0743
Pavel Konopelko <pkonopel@visteon.com>
2015-09-30 20:05:08 +0200

    tools: initial commit of the project infrastructure
    Eclipse-based tooling relies on Maven and Tycho.  The initial setup supports
    building two plugins:
    
    * org.genivi.capic.core encapsulates the generator functionality (currently
      a dummy stub)
    
    * org.genivi.capic.core.ui extends Eclipse workbench with a popup menu entry
      for .fidl files to invoke the generator
    
    Other subprojects provide the required Maven-specific build infrastructure.
    
    UI adds a context menu entry in the Project Explorer for any selected file
    and a keyboard shortcut <Alt-Ctrl-C> to invoke the generator.
    
    Signed-off-by: Pavel Konopelko <pkonopel@visteon.com>

---
+++ tools/org.genivi.capic.core.ui/src/org/genivi/capic/core/ui/handlers/GenerateCCodeHandler.java
@@ -0,0 +1,59 @@
+/* SPDX license identifier: MPL-2.0
+ * Copyright (C) 2015, Visteon Corp.
+ * Author: Pavel Konopelko, pkonopel@visteon.com
+ *
+ * This file is part of Common API C
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License (MPL), version 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * you can obtain one at http://mozilla.org/MPL/2.0/.
+ * For further information see http://www.genivi.org/.
+ */
+package org.genivi.capic.core.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.genivi.capic.core.Generator;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class GenerateCCodeHandler extends AbstractHandler {
+    /**
+     * The constructor.
+     */
+    public GenerateCCodeHandler() {
+    }
+
+    /**
+     * the command has been executed, so extract extract the needed information
+     * from the application context.
+     */
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+        ISelectionService service = window.getSelectionService();
+        if (!(service.getSelection() instanceof IStructuredSelection)) {
+            return null;
+        }
+        IStructuredSelection selection = (IStructuredSelection) service.getSelection();
+        if (!(selection.getFirstElement() instanceof IFile)) {
+            return null;
+        }
+        Generator generator = new Generator((IFile) selection.getFirstElement());
+        MessageDialog.openInformation(
+                window.getShell(),
+                "Common API C Code Generator UI",
+                "Generating Common API C Code for\n" + generator.generate());
+        return null;
+    }
+}
