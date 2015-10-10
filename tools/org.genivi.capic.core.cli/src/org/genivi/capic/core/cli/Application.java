b8655795d239ae20f6753fb8aba0b7939cb8d0c6
Pavel Konopelko <pkonopel@visteon.com>
2015-10-10 20:40:55 +0200

    tools: extend core.cli to open file and pass it to Generator
    Invoking the Generator requires that the core plugin can be accessed.  This
    is achieved by extending core.cli.feature to include it.  The bundle
    org.eclipse.core.resources (which is required to manipulate Eclipse
    workspace and projects) must be included into the feature as well.
    
    In order to pass an instance of IFile to Generator, the corresponding input
    file must be linked into the default workspace that is opened upon the
    startup of the rcp application.  The corresponding Eclipse functionality
    relies on absolute paths.  Therefore the generator binary expects one
    command line argument and it must be an existing absolute path.  Any changes
    to the default workspace must be reverted.  Otherwise subsequent launches
    with the same argument will result into 'overlapping resource' errors
    because the file has been already linked.
    
    As the result:
    
    * core.cli plugin launches clean from the ide using launch configuration
      'org.genivi.capic.core.cli.appid'.
    
    * core.product does NOT launch from the ide and the exported binary does not
      launch either due to unresolved org.eclipse.resources; in the latter case
      config.ini does not include org.eclipse.core.resources in its osgi.bundles
      entry; the failure is consistent regardless what target platform is active
      ('org.genivi.capic.core.target' or 'Running Platform'); both target
      platforms include org.eclipse.resources in their content.
    
    * core.product binaries built with maven launch without error on both linux
      and windows.
    
    Signed-off-by: Pavel Konopelko <pkonopel@visteon.com>

--- tools/org.genivi.capic.core.cli/src/org/genivi/capic/core/cli/Application.java
+++ tools/org.genivi.capic.core.cli/src/org/genivi/capic/core/cli/Application.java
@@ -12,10 +12,23 @@
  */
 package org.genivi.capic.core.cli;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.genivi.capic.core.Generator;
 
 public class Application implements IApplication {
+
+    static private String usageText = "Usage:\ncapic-core-gen <fidl-file>";
 
     @Override
     public Object start(IApplicationContext context) throws Exception {
@@ -24,6 +37,49 @@ public class Application implements IApplication {
             IApplicationContext.APPLICATION_ARGS);
         for (final String arg : appArgs)
             System.out.println(arg);
+        if (appArgs.length != 1) {
+            System.out.println("Illegal number of arguments");
+            System.out.println(usageText);
+            return IApplication.EXIT_OK;
+        }
+
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot root = workspace.getRoot();
+        System.out.println("WorkspaceRoot.Location = " + root.getLocationURI());
+
+        IProject project = root.getProject("CAPIC Temporary");
+        if (!project.exists())
+            project.create(null);
+        if (!project.isOpen())
+            project.open(null);
+
+        IPath location = new Path(appArgs[0]);
+        IFile file = project.getFile(location.lastSegment());
+        IStatus status = workspace.validateLinkLocation(file, location);
+        if (!status.isOK()) {
+            System.out.println("Invalid file location: " + status.getMessage());
+            return IApplication.EXIT_OK;
+        }
+
+        try {
+            file.createLink(location, IResource.REPLACE, null);
+        } catch (CoreException e) {
+            System.out.println("Unable to link file: " + e.getMessage());
+            return IApplication.EXIT_OK;
+        }
+        System.out.println("File.Location = " + file.getLocation() + "\nFile.FullPath = " + file.getFullPath());
+
+        Generator generator = new Generator(file);
+        System.out.println("Generating Common API C Code for\n" + generator.generate());
+
+        project.close(null);
+        project.delete(IResource.FORCE | IResource.ALWAYS_DELETE_PROJECT_CONTENT, null);
+        try {
+            workspace.save(true, null);
+        } catch (CoreException e) {
+            // silently ignore failure to save the workspace
+        }
+
         return IApplication.EXIT_OK;
     }
 
