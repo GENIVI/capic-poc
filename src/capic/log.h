bc6212967a8d29420c2e843b2b99b4c1ad2bcac1
Pavel Konopelko <pkonopel@visteon.com>
2016-03-11 16:49:13 +0100

    capic, ref: make logging output optional
    libcapic implements logging infrastructure that it used by the library
    code itself as well as by the generated code and the code in the
    reference and test applications.  The logging is implemented via macros
    and is enabled by default.
    
    The build configuration option --disable-logging can now be used to make
    these macros void (and thus prevent any logging code from executing).
    In libcapic, the symbol ENABLE_LOGGING is defined via config.h.  Since
    the generated binding code does not include config.h, this symbol must
    be defined via the preprocessor options when building them.  All
    reference and test applications support the option --disable-logging
    too.  They also define the corresponding symbol via the preprocessor
    options.
    
    Signed-off-by: Pavel Konopelko <pkonopel@visteon.com>

--- src/capic/log.h
+++ src/capic/log.h
@@ -1,5 +1,5 @@
 /* SPDX license identifier: MPL-2.0
- * Copyright (C) 2015, Visteon Corp.
+ * Copyright (C) 2015-2016, Visteon Corp.
  * Author: Pavel Konopelko, pkonopel@visteon.com
  *
  * This file is part of Common API C
@@ -13,6 +13,8 @@
 
 #ifndef INCLUDED_CC_LOG
 #define INCLUDED_CC_LOG
+
+#if ENABLE_LOGGING
 
 #define WITH_SYSTEMD_JOURNAL
 #ifdef WITH_SYSTEMD_JOURNAL
@@ -35,6 +37,15 @@
 #define CC_LOG_CLOSE() closelog()
 #define CC_LOG_ERROR(...) syslog(LOG_ERR, __VA_ARGS__)
 #define CC_LOG_DEBUG(...) syslog(LOG_DEBUG, __VA_ARGS__)
+
+#endif
+
+#else
+
+#define CC_LOG_OPEN(program) ((void)(program))
+#define CC_LOG_CLOSE() ((void)0)
+#define CC_LOG_ERROR(...) ((void)0)
+#define CC_LOG_DEBUG(...) ((void)0)
 
 #endif
 
