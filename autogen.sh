0149c512d60fb03de19746d73df19e49b3c40f25
Pavel Konopelko <pkonopel@visteon.com>
2016-07-05 19:55:43 +0200

    capic: introduce autogen script
    Signed-off-by: Pavel Konopelko <pkonopel@visteon.com>

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# SPDX license identifier: MPL-2.0
+# Copyright (C) 2016, Visteon Corp.
+# Author: Pavel Konopelko, pkonopel@visteon.com
+#
+# This file is part of Common API C
+#
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License (MPL), version 2.0.
+# If a copy of the MPL was not distributed with this file,
+# you can obtain one at http://mozilla.org/MPL/2.0/.
+# For further information see http://www.genivi.org/.
+
+set -e
+
+test -d m4 || mkdir m4
+autoreconf --install --verbose --force
